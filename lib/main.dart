27227ddd0d63447fc198f1629e8e5356678c9477
Killian Habasque <killian.habasque@gmail.com>
2025-04-16 14:41:13 +0200

    change home/ routes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:init/features/movies/data/datasources/movie_remote_datasource.dart';
+import 'package:init/features/movies/data/repositories/movie_repository_impl.dart';
+import 'package:init/features/movies/domain/usecases/get_popular_movies.dart';
+import 'package:init/features/movies/presentation/pages/movie_home_screen.dart';
+import 'package:init/features/movies/presentation/providers/movie_provider.dart';
+import 'package:provider/provider.dart';
 import 'features/auth/presentation/screens/login_screen.dart';
 import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/data/services/auth_service.dart';
@@ -8,7 +14,20 @@ import 'core/constants.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
-  runApp(const MyApp());
+  final movieRemoteDataSource = MovieRemoteDataSource();
+  final movieRepository = MovieRepositoryImpl(movieRemoteDataSource);
+  final getPopularMovies = GetPopularMovies(movieRepository);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MovieProvider(getPopularMovies)..fetchMovies(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -25,15 +44,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginScreen(
-        authRepository: authRepository,
-        authService: authService,
-      ),
+      home: MovieHomeScreen(),
       routes: {
-        '/login': (context) => LoginScreen(
-          authRepository: authRepository,
-          authService: authService,
-        ),
+        '/login':
+            (context) => LoginScreen(
+              authRepository: authRepository,
+              authService: authService,
+            ),
       },
     );
   }
